7042c16ecdb460d7c38949aeff0ab88884e8b24a
Andrew Holt <ajh.206@outlook.com>
2019-06-20 14:15:35 -0700

    Create MissingNumber1-10.py
    Level1 FireCode Solution

---
+++ Level_1/MissingNumber1-10.py
@@ -0,0 +1,14 @@
+"""
+Given an list containing 9 numbers ranging from 1 to 10, write a function to find the missing number. 
+Assume you have 9 numbers between 1 to 10 and only one number is missing.
+
+Example:
+input_list: [1, 2, 4, 5, 6, 7, 8, 9, 10]
+find_missing_number(input_list) => 3
+"""
+```Python3
+def find_missing_number(list_numbers):
+    ranger = list(range(1,11))
+    solution = [int(x) for x in ranger if x not in list_numbers]
+    return solution[0]
+```
